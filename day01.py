1bfd470ebfbdd5e8388319f77408f451ebbe5fab
DavidCalebChaparroOrozco <47438665+DavidCalebChaparroOrozco@users.noreply.github.com>
2023-12-04 00:13:57 -0500

    Create day01.py

---
+++ day01.py
@@ -0,0 +1,13 @@
+print('David Caleb Chaparro Orozco') ## David Caleb Chaparro Orozco
+
+print("""
+I am signing up for Replit's 100 days of Python challenge!
+I will make sure to spend some time every day coding along, for a minimum of 10 minutes a day.
+I'll be using Replit, an amazing online IDE so I can do this from my phone wherever I happend to be. No excuses for not coding from the middle of a field!
+""") 
+## I am signing up for Replit's 100 days of Python challenge!
+## I will make sure to spend some time every day coding along, for a minimum of 10 minutes a day.
+## I'll be using Replit, an amazing online IDE so I can do this from my phone wherever I happend to be. No excuses for not coding from the middle of a field!
+
+
+print("I am feeling a bit a tired 😅") # I am feeling a bit a tired 😅
