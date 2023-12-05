824f6944e12fe857b6ef5bba2082cf46035668c5
DavidCalebChaparroOrozco <47438665+DavidCalebChaparroOrozco@users.noreply.github.com>
2023-12-05 17:24:07 -0500

    Create day03.py

---
+++ day03.py
@@ -0,0 +1,31 @@
+#myName = input("What's your name? ")
+#myLunch = input("What are you having for lunch? ")
+#print(myName, "is going to be chowing down on", myLunch, "very soon!")
+
+#number = input("Give me a number: ")
+#group = input("Give me a collective noun for a group of things: ")
+#thing = input("Give me the name of a weird or wacky thing: ")
+#print("No I don't think that", number, "is a", group, "of", thing,". That's just odd.")
+
+#yourName = input("Name: ")
+#whatYear = input("What year is it?: ")
+#print(yourName, "thinks it is", whatYear)
+
+#print("=== Your Song Generator ===")
+#print("""You'll be asked a bunch of questions then we'll make you up an amazing song, totally copyright free 😭""")
+#print()
+#person = input("Name a person famous for something good: ")
+#thing = input("Name a thing they did: ")
+#place = input("Name a place you like: ")
+#rhyme = input("Give me a verb that rhymes with your person's name: ")
+#print()
+#print("There was a person called",person)
+#print("Who did something cool like", thing, "at the wonderful", place ,"where you'll find me", rhyme)
+
+typeFood = input("Name a food: ")
+typePlant = input("Name a type of plant: ")
+methodCook = input("Name a method of cooking: ")
+burnedFood = input("What word describes burned food? ")
+diyItem = input("Name a DIY item")
+
+print("Your Menu:", methodCook, "&" ,typeFood, "with", burnedFood, typePlant, "on a bed of Hammers")
