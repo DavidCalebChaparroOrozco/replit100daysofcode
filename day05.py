680db915abeaef42938d020dae799d93dd50af09
DavidCalebChaparroOrozco <47438665+DavidCalebChaparroOrozco@users.noreply.github.com>
2023-12-07 14:38:55 -0500

    Create day05.py

---
+++ day05.py
@@ -0,0 +1,44 @@
+#myName = input("What's your name?: ")
+#if myName == "Caleb":
+#  print("Welcome Dude!")
+#  print("You're just the baldest dude I've ever seen")
+#else:
+#  print("Who on earth are you?!")
+
+#catsOrDogs = input("Are you a cat person? Or a dog person?: ")
+#if catsOrDogs == "cat":
+#  print("Meow")
+#else:
+#  print("Woof")
+
+
+#drink = input("Do you prefer coffee or tea?: ")
+#if drink == "coffee":
+#  print("Tea is better.")
+#else:
+#  print("Excellent choice.")
+
+
+print("Which character are you from 'Avengers?'")
+print()
+print("Answer these questions and let's find out.")
+print()
+print("Please use Y or N for yes and no.")
+
+likeGreen = input("Do you like the color green?: ")
+if likeGreen == "Y":
+  print("You must be the Hulk!")
+
+IronIsCool = input("Do you think building robots is cool?: ")  
+if IronIsCool == "Y":
+  print("You must be Iron Man. Cool suit!")
+
+TimeTravel = input("Do you like traveling through time?: ")  
+if TimeTravel == "Y":
+  print("You must be Captain America!")
+
+Strong = input("Are you super strong?: ")
+if Strong == "Y":
+  print("You have got to be Thor!")
+else:
+  print("I guess you are not like anyone from 'Avengers.'")
