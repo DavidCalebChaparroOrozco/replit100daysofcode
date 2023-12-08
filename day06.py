236dd3c21bce6a599bc4d37920fffcceacb0249e
DavidCalebChaparroOrozco <47438665+DavidCalebChaparroOrozco@users.noreply.github.com>
2023-12-08 15:28:58 -0500

    Create day06.py

---
+++ day06.py
@@ -0,0 +1,50 @@
+# print("SECURE LOGIN")
+# username = input("Username > ")
+# if username == "Caleb":
+#   print("Welcome Caleb!")
+# else:
+#   print("Go away!")
+
+# print("SECURE LOGIN")
+# username = input("Username: ")
+# password = input("Password: ")
+
+# if username == "Caleb" and password == "password":
+#  print("Welcome Caleb!")
+# elif username == "suzanne" and password == "Su74nne":
+#  print("Hey there Suzanne!")
+# else:
+#  print("Go away!")
+
+# print("SECURE LOGIN")
+# username = input("Username: ")
+# if username == "Caleb":
+#   print("Welcome Caleb!")
+# elif username == "suzanne":
+#   print("Hey there Suzanne!")
+# else:
+#   print("Go away!")
+
+# Commented code
+# season = input("What is your favorite season?: ")
+# if season == "spring":
+#   print("Ah! The birds are chirping and flowers blooming.")
+# elif season == "summer":
+#   print("Catch some sun and cool off with a lemonade.")
+# elif season == "autumn":
+#   print("The leaves are changing and the air is crisp. Enjoy!")
+# elif season == "winter":
+#       print("Stay warm by the fire and watch the snow fall.")
+# else: 
+#   print("I don't know that season. Please try again.")
+
+print("Login")
+username = input("Enter your username: ")
+password = input("Enter your password: ")
+
+if username == "Caleb" and password == "passwordtest":
+  print("Welcome Caleb! You have a nice day!")
+elif username == "David" and password == "passwordtest1":
+  print("Welcome David! You have a nice day!")
+else:
+  print("You do not have access. Go away!")
