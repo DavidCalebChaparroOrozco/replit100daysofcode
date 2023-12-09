00801b7650f03544673b0db13fafd7adecaf5b30
DavidCalebChaparroOrozco <47438665+DavidCalebChaparroOrozco@users.noreply.github.com>
2023-12-09 14:46:17 -0500

    Create day07.py

---
+++ day07.py
@@ -0,0 +1,63 @@
+# TVshow = input("What is your favorite tv show? ")
+# if TVshow == "Futurama":
+#   print("Ugh, why?")
+#   faveCharacter = input("Who is your favorite character? ")
+#   if faveCharacter == "Bender":
+#     print("Right answer")
+#   else:
+#     print("Nah, Bender the greatest")
+# elif TVshow == "The Simpsons":
+#   print("Aww, sad times")
+# else:
+#   print("Yeah, that's cool and all…")
+
+
+
+# TVshow = input("What is your favourite tv show? ")
+# if TVshow == "Futurama":
+#   print("Ugh, why?")
+#   faveCharacter = input("Who is your favourite character? ")
+#   if faveCharacter == "Bender":
+#     print("Right answer")
+#   else:
+#     print("Nah, Daddy Pig's the greatest")
+# elif TVshow == "The Simpsons":
+#   print("Aww, sad times")
+# else:
+#   print("Yeah, that's cool and all…")
+
+
+
+# order = input("What would you like to order: pizza or hamburger? ")
+# if order == "hamburger":
+#   print("Thank you.")
+#   cheese = input("Do you want cheese?: ")
+#   if cheese == "yes":
+#     print("You got it.")
+#   else: 
+#     print("No cheese it is.")
+# elif order == "pizza":
+#   print("Pizza coming up.")
+#   toppings = input("Do you want pepperoni on that?: ")
+#   if toppings == "yes":
+#     print("We will add pepperoni.")
+# else:
+#   print("Your pizza will not have pepperoni.")
+
+
+
+
+print ("Are you a superfan of 'Rick And Morty' or a fake fan?")
+print()
+print("Answer these questions to find out.")
+
+Glasses = input("Beth Sanchez is a horse surgeon? :")
+if Glasses == "yes":
+  print("Correct!")
+else:
+  print("Wrong!")
+  WhoGlasses = input("And what is the first word of the first episode? :")
+  if WhoGlasses == "Rick":
+    print("You got it")
+  else:
+    print("Try again!")
