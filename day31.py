b1ef5a42b7e74f8004493892bf73025721c7cb68
DavidCalebChaparroOrozco <47438665+DavidCalebChaparroOrozco@users.noreply.github.com>
2024-01-02 15:47:26 -0500

    Create day31.py

---
+++ day31.py
@@ -0,0 +1,44 @@
+def colorChange(color):
+   if color == "red":
+       return ("\033[31m")
+   elif color == "white":
+       return ("\033[0m")
+   elif color == "blue":
+       return ("\033[34m")
+   elif color == "yellow":
+       return ("\033[33m")
+   elif color == "green":
+       return ("\033[32m")
+   elif color == "purple":
+       return ("\033[35m")
+
+title = f"{colorChange('red')}{colorChange('white')}{colorChange('blue')} {colorChange('yellow')}Rock Station {colorChange('blue')}{colorChange('white')}{colorChange('red')}"
+
+print(f"        {title:^45}")
+print(f"▶️\t{colorChange('white')}Back in Black")
+print(f"\t{colorChange('yellow')}AC/DC")
+
+prev = " Prev"
+next = " Next "
+pause = " Pause "
+
+print(f"{colorChange('white')}{prev:<35}")
+print(f"{colorChange('green')}{next:^35}")
+print(f"{colorChange('purple')}{pause:>35}")
+
+text = "WELCOME TO"
+print(f"{colorChange('white')}{text:^35}")
+text = "--  ARMBOOK  --"
+print(f"{colorChange('blue')}{text:^35}")
+text = "Definitely not a rip off"
+print(f"{colorChange('yellow')}{text:>35}")
+text = "a certain other social"
+print(f"{colorChange('yellow')}{text:>35}")
+text = "networking site"
+print(f"{colorChange('yellow')}{text:>35}")
+text = "Honest."
+print(f"{colorChange('red')}{text:^35}")
+text = "Username: "
+username = input(f"{colorChange('white')}{text:^35}")
+text = "Password: "
+username = input(f"{colorChange('white')}{text:^35}")
