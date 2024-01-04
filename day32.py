c5eb809e0ddd8d3d9255ea64230d613b328cb983
DavidCalebChaparroOrozco <47438665+DavidCalebChaparroOrozco@users.noreply.github.com>
2024-01-03 23:46:49 -0500

    Create day32.py

---
+++ day32.py
@@ -0,0 +1,31 @@
+import random
+# timetable = ["Computer Science", "Math", "English", "Art", "Sport"]
+# print(timetable[1])
+
+
+# timetable = ["Computer Science", "Math", "English", "Art", "Sport"]
+# timetable[4]= "Watch TV"
+# print(timetable[0])
+# print(timetable[1])
+# print(timetable[2])
+# print(timetable[3])
+# print(timetable[4])
+
+
+# timetable = ["Computer Science", "Math", "English", "Art", "Watch TV"]
+# for lesson in timetable:
+#   print(lesson)
+
+
+# colors = ["Red", "Orange", "Yellow", "Green", "Blue", "Violet"]
+# print(f"The first color is {colors[0]}")
+# print(f"The first color is {colors[5]}")
+
+
+# grocery_list = ["bananas", "bread", "milk", "eggs", "juice", "cheese"]
+# print(f"The first grocery item to buy is {grocery_list[0]}.")
+
+
+greetings = ["Hola Mundo","Hello there!", "Konnichiwa", "Guten Tag!", "Bore Da!"]
+index = random.randint(0, len(greetings) - 1)
+print(greetings[index])
