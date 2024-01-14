77a06068bb1bb87517f4ddada1e7cbcbc3107a1c
DavidCalebChaparroOrozco <47438665+DavidCalebChaparroOrozco@users.noreply.github.com>
2024-01-14 11:45:17 -0500

    Create day42.py

---
+++ day42.py
@@ -0,0 +1,27 @@
+mokedex = {
+  "Beast Name": None, 
+  "Type": None, 
+  "Special Move": None, 
+  "HP": None, 
+  "MP":None
+}
+
+print("MokéBeast")
+
+for name, value in mokedex.items():
+  mokedex[name] = input(f"{name}:\t").strip().title()
+
+if mokedex["Type"]=="Earth":
+  print("\033[32m", end="")
+elif mokedex["Type"]=="Air":
+  print("\033[37m", end="")
+elif mokedex["Type"]=="Fire":
+  print("\033[31m", end="")
+elif mokedex["Type"]=="Water":
+  print("\033[34m", end="")
+else:
+  print("\033[33m", end="")
+
+for name, value in mokedex.items():
+  print(f"{name:<15}: {value}")
+  
